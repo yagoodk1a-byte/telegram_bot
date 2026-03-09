c78c3d7d30b30d3877e1d49868b5031b3a28913c
yagoodk1a-byte <yagoodk1a@gmail.com>
2026-03-09 19:43:26 +0800

    Update new1.py
    роспись лотов

--- new1.py
+++ new1.py
@@ -1,12 +1,63 @@
 
 import re
 import telebot
+import re
+
+import re
+
+lots = {
+
+# 400
+"7/400": [150,100,50],
+"10/400": [150,50,50],
+"14/400": [200,50,50],
+"17/400": [200,150,50],
+"20/400": [250,250,50],
+"24/400": [250,250,50],
+"28/400": [350,300,50],
+"33/400": [350,300,50],
+"37/400": [350,300,50],
+"41/400": [400,400,50],
+"42/400": [400,400,50],
+"45/400": [400,400,50],
+"50/400": [400,400,150],
+
+# 600
+"16/600": [200,150,50],
+"26/600": [350,300,50],
+"36/600": [350,300,50],
+"46/600": [400,400,50],
+
+# 1000
+"11/1000": [200,150,50],
+"15/1000": [150,100,50],
+"20/1000": [300,300,50],
+"25/1000": [300,300,150],
+"30/1000": [300,300,150],
+"40/1000": [400,400,150],
+
+# 1500
+"17/1500": [200,200,100],
+"27/1500": [350,350,150],
+"37/1500": [400,400,150],
+
+# 2000
+"15/2000": [300,250,50],
+"20/2000": [300,300,150],
+"25/2000": [300,300,150],
+"30/2000": [400,400,150],
+"35/2000": [400,400,150],
+}
+
+
+def calculate_chests(text):
+
 
 bot=telebot.TeleBot("8358575962:AAFscZFM5pElS0AWZs_1kBZFnH-NyLwf034")
 
 @bot.message_handler(commands=['start'])
 def main(message):
-    bot.send_message(message.chat.id, f"Привет, {message.from_user.first_name}, можешь отправить мне любое сообщение с текстом и числами, я все посчитаю!")
+    bot.send_message(message.chat.id, f"Привет, {message.from_user.first_name}, теперь я не только могу посчитать баланс и сумму карт, но и расписывать твои лоты!")
 
 
 @bot.message_handler(func=lambda message: message.text and message.text.lower() == "привет")
@@ -14,6 +65,13 @@ def info(message):
     if message.text.lower() == "привет":
         bot.send_message(message.chat.id, f"Привет, {message.from_user.first_name}, можешь отправить мне любое сообщение с текстом и числами, я все посчитаю!")
 
+@dp.message()
+async def handler(message: types.Message):
+
+    result = calculate_chests(message.text)
+
+    if result:
+        await message.answer(result)
 
 
 @bot.message_handler(func=lambda message: True)
@@ -45,3 +103,4 @@ def handle_message(message):
 
 
 bot.polling(none_stop=True)
+
